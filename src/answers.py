e994bae3d020c55ef251f7641ff9163a4fef30de
Lucas Maretti <lucas.maretti@gmail.com>
2023-02-27 14:54:59 -0300

    Respondidas perguntas 9, 13 e 14 da rd1

---
+++ src/answers.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+def rd1_question_9(df):
+
+    return None
+
+
+def rd1_question_13(df):
+
+    return None
+
+
+def rd1_question_14(df):
+
+    return None
